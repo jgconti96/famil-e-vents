ff589a140f047a409ce109d3e156c42588caec19
john <jgconti96@gmail.com>
2018-07-26 20:48:06 -0400

    initial commit

---
+++ android/app/src/main/java/com/famil_e_vents/MainActivity.java
@@ -0,0 +1,15 @@
+package com.famil_e_vents;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "famil_e_vents";
+    }
+}
